11ce84dba6c20e1483589f21368f551974983399
shantanusutar1-star <shantanusutar1@gmail.com>
2026-08-04 04:13:21 +0000

    exp2.cpp

---
+++ exp2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class student{
+    private:
+        string name;
+        int rollno;
+        float marks;
+
+    public:
+       void inputDetails(){
+
+        cout<<"Enter Student Name: ";
+        getline(cin>>ws,name);
+        cout<<"Enter Roll No. ";
+        cin>>rollno;
+        cout<<"Enter Marks: ";
+        cin>>marks;
+    }
+
+    void displayDetails()const{
+
+        cout<<"\n-----Student DEtails-----\n";
+        cout<<"Name: "<<name<<endl;
+        cout<<"Roll No. : "<<rollno<<endl;
+        cout<<"Marks:  "<<marks<<endl;
+    }
+           
+};
+
+int main(){
+
+    student s;
+    s.inputDetails();
+    s.displayDetails();
+    return 0;
+}
