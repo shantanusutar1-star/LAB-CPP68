2ddd94fbb9474b0147912ac9202df17eb25ed33d
shantanusutar1-star <shantanusutar1@gmail.com>
2026-08-11 04:59:08 +0000

    exp3N1.cpp

--- exp3.cpp
+++ exp3.cpp
@@ -9,11 +9,12 @@ class Complex{
     public:
         Complex(int r=0,int i=0):real(r),imag(i){}
         
-        Complex add(Complex x1,Complex x2){
-            Complex x3;
-            x3.real = x1.real + x2.real;
-            x3.imag = x1.imag + x2.imag;
-            return x3;
+        int add(Complex x1){
+        
+            real = real + x1.real;
+            imag = imag + x1.imag;
+            return 0;
+        
         }
 
         Complex subtract(const Complex &c){
@@ -27,12 +28,13 @@ class Complex{
 
 int main(){
     Complex c1(4,5),c2(8,9);
-    Complex sum=c1.add(c1,c2);
+    c1.add(c2);
     Complex diff=c1.subtract(c2);
 
     cout<<"First Complex Number: ";c1.display();
     cout<<"Second Complex Number: ";c2.display();
-    cout<<"Addition: ";sum.display();
+    cout<<"Addition: ";c1.
+    display();
     cout<<"Subtraction: ";diff.display();
 
     return 0;
